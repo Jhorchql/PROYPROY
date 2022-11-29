ae1fb05e77a1efef2df2a3faf9d40b70d2901993
Jhorchql <117998365+Jhorchql@users.noreply.github.com>
2022-11-28 20:08:10 -0500

    Update proebando.py

--- proebando.py
+++ proebando.py
@@ -1,46 +1,46 @@
-importar  streamlit  como  st
-importar  pandas  como  pd
-importar  numpy  como  np
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-#título
-st . título ( 'Índices Soberanos 2010 - 2022' )
-st . subtítulo ( "Miembros del equipo" )
-st . descuento ( """
+#Título
+st.title('Índices Soberanos 2010 - 2022')
+st.subheader("Miembros del equipo")
+st.markdown("""
 - Palacios Ninahuanca, Ninoska
--Orozco Chupos, Frank
--Quispe Laura, Jhorch
-- Parillo Sánchez, Yassmin Diana
-""" )
-st . descuento ( """
+- Orozco Chupos, Frank
+- Quispe Laura, Jhorch
+- Parillo Sanchez, Yassmin Diana
+""")
+st.markdown("""
 ---
-La información contenida en esta página web permite acceder al Dataset “Índices Soberanos 2010 - 2022”
-elaborado por el Ministerio de Economía y Finanzas del Perú.
+La información contenida en esta página web permite acceder al Dataset “Índices Soberanos 2010 - 2022” 
+elaborado por el Ministerio de Economía y Finanzas del Perú. 
 Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/%C3%ADndices-soberanos-2010-2022)
 ---
-""" )
-def  cargar_datos ():
-    url = "https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
-    devolver  p.d. _ leer_csv ( url )
-st . casilla de verificación ( "Usar ancho de contenedor" , valor = Falso , clave = "use_container_width" )
+""")
+def load_data():
+    url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
+    return pd.read_csv(url)
+st.checkbox("Use container width", value=False, key="use_container_width")
 
-df  =  cargar_datos ()
-st . marco de datos ( df , use_container_width = st . session_state . use_container_width )
-url = "https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
-c = pd . leer_csv ( url )
-#comparacion del indice nominal e indice real
-df  =  c . drop ( columnas  = [ "FECHA" , "INDICE_NOMINAL" , "INDICE_REAL" ])
-st . marco de datos ( df , use_container_width = st . session_state . use_container_width )
-st . gráfico_de_líneas ( df )
+df = load_data()
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
+url="https://raw.githubusercontent.com/Frank10OC/proyecto/main/indices_soberanos%20(1).csv"
+c=pd.read_csv(url)
+#comparación del Índice nominal e Índice real 
+df = c.drop(columns = ["FECHA","INDICE_NOMINAL","INDICE_REAL"])
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
+st.line_chart(df)
 #"INDICE_NOMINAL","RENT_ANUAL_IN"
-i1 =  c . drop ( columnas  = [ "FECHA" , "INDICE_REAL" , "RENT_ANUAL_IR" ])
-st . marco de datos ( i1 , use_container_width = st . session_state . use_container_width )
+i1= c.drop(columns = ["FECHA","INDICE_REAL","RENT_ANUAL_IR"])
+st.dataframe(i1, use_container_width=st.session_state.use_container_width)
 #"INDICE_NOMINAL","RENT_ANUAL_IN"
-i2 =  c . drop ( columnas  = [ "FECHA" , "INDICE_NOMINAL" , "RENT_ANUAL_IN" ])
-st . marco de datos ( i2 , use_container_width = st . session_state . use_container_width )
+i2= c.drop(columns = ["FECHA","INDICE_NOMINAL","RENT_ANUAL_IN"])
+st.dataframe(i2, use_container_width=st.session_state.use_container_width)
 
-df1 =  c
-set_fecha =  np . ordenar ( df1 [ "FECHA" ]. dropna (). único ())
+df1= c
+set_fecha= np.sort(df1["FECHA"].dropna().unique())
 #Seleccion del departamento
-opcion_fecha  =  st . selectbox ( 'Selecciona una fecha' , set_fecha )
-df_fecha  =  df1 [ df1 [ "FECHA" ] ==  opcion_fecha ]
-num_filas  =  len ( df_fecha . ejes [ 0 ])
+opcion_fecha = st.selectbox('Selecciona una fecha', set_fecha)
+df_fecha = df1[df1["FECHA"] == opcion_fecha]
+num_filas = len(df_fecha.axes[0])
